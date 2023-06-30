eeb3b541c761d6efc6b897b3a7674987ca73876f
JustinEvans-github <53385587+JustinEvans-github@users.noreply.github.com>
2023-06-30 19:49:17 -0400

    webscrape runs on working route

--- app/routes.py
+++ app/routes.py
@@ -11,44 +11,46 @@ from flask import render_template, request, redirect, url_for, flash
 def base():
     return render_template('base.html')
 
-@app.route('/',methods=['GET','POST'])
-def add_task():
-
+@app.route('/webscrape',methods=['GET','POST'])
+def webscrape():
     # extract page contents
     page_num = random.randint(0, 40)
     url_start = f'https://books.toscrape.com/catalogue/page-{page_num}.html'
 
     # define quiz contents
     content = book_page(url_start)
+    global title_options
     title_options = [content[0][0],content[1][0],content[2][0]]
+    print(f'Answer: {content[0][0]}')
 
     global description_answer
     description_answer = content[0][1]
-    title_answer = content[0][0]
+
+    return redirect(url_for('add_task'))
+
+@app.route('/',methods=['GET','POST'])
+def add_task():
 
     # define quiz content in WTForm
     #form = PopQuiz()
-    global form
     form = RadioQuiz()
     form.q1.choices = [('False', title_options[0]), ('True', title_options[1]),(False,title_options[2])]
     form.q1.validator = [CorrectAnswer('False')]
-
-
 
     if form.is_submitted():
         print ("submitted")
         print(form.errors)
     if form.validate():
         print("validate")
+        print(f'form.data {form.data}')
 
     if form.validate_on_submit():
         print("validated on submit")
-        flash('Thanks for registering')
         return redirect(url_for('passed'))
     return render_template('add-task.html', form=form, description_answer=description_answer)
 
     
 @app.route('/passed/')
 def passed():
-    print("switched to success")
+    flash('Correct Answer')
     return render_template('passed.html', form=form, description_answer=description_answer)
